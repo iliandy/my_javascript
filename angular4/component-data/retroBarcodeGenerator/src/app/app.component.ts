6cb271eb8e8e393f9bce3e7c8d1956466e632cab
iliandy <iandyli@gmail.com>
2017-08-06 19:29:37 -0500

    completed retroBarcodeGenerator assignment

---
+++ angular4/component-data/retroBarcodeGenerator/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  colors = ["DodgerBlue", "Gold", "LightGray", "LimeGreen", "Magenta", "OrangeRed", "RoyalBlue", "Orange"];
+
+  shuffle(array) {
+    let counter = array.length;
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+        // Decrease counter by 1
+        counter--;
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+    return array;
+  }
+
+  ngOnInit() {
+    this.shuffle(this.colors);
+  }
+}
