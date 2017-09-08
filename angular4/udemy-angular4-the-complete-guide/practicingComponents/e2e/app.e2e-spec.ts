ead492842d90501e5b23084b6cf65214a997535e
iliandy <iandyli@gmail.com>
2017-09-08 16:29:07 -0500

    completed udemy practicingComponents assignment again

--- angular4/udemy-angular4-the-complete-guide/practicingComponents/e2e/app.e2e-spec.ts
+++ angular4/udemy-angular4-the-complete-guide/practicingComponents/e2e/app.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { PracticingComponentsPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('practicing-components App', () => {
-  let page: PracticingComponentsPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new PracticingComponentsPage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
