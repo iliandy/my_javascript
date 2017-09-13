8ddc19c50cb291556d87d735511f2c9cb763958b
iliandy <iandyli@gmail.com>
2017-09-13 18:38:38 -0500

    ngRecipes project: created header, recipe model, display recipe list

--- angular4/udemy-angular4-the-complete-guide/ngRecipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular4/udemy-angular4-the-complete-guide/ngRecipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Easy Pressure Cooker Pot Roast", "Yummy tender beef, carrots, and potatos!", "http://images.media-allrecipes.com/userphotos/720x405/3553277.jpg"),
+    new Recipe("Pressure Cooker Beef Chili", "Yummy tender beefy chili!", "http://images.media-allrecipes.com/userphotos/720x405/961322.jpg")
+  ];
 
   constructor() { }
 
