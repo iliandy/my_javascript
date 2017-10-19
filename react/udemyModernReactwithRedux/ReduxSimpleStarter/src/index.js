4a7cf5e9bf60e7dce31634e9ee3aa6dd0b38fb95
iliandy <iandyli@gmail.com>
2017-10-19 00:08:20 -0500

    Udemy MRwR: created search_bar.js component, added into index.js

--- react/udemyModernReactwithRedux/ReduxSimpleStarter/src/index.js
+++ react/udemyModernReactwithRedux/ReduxSimpleStarter/src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SearchBar from "./components/search_bar";
+
+const API_KEY = "AIzaSyD-sJMng531xBH4TcTuK-pud4OxSg74GrU";
 
 // create component to generate HTML
 const App = () => {
-  return <div>Hi world!</div>;
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  );
 }
 
 // place component generated HTML and put it into page/DOM
