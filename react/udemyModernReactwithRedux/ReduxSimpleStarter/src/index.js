6a75c7345aeef6dd2fee98818857ce6678a80e6f
iliandy <iandyli@gmail.com>
2017-10-20 22:20:03 -0500

    Udemy MRwR: completed up through 1.20 Breather and Review, converted SearchBar from return function to class.

--- react/udemyModernReactwithRedux/ReduxSimpleStarter/src/index.js
+++ react/udemyModernReactwithRedux/ReduxSimpleStarter/src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 
 const API_KEY = "AIzaSyD-sJMng531xBH4TcTuK-pud4OxSg74GrU";
+
+YTSearch({ key: API_KEY, term: "Pixel 2 XL"}, function(data) {
+  console.log(data);
+});
 
 // create component to generate HTML
 const App = () => {
   return (
     <div>
-      <SearchBar/>
+      <SearchBar />
     </div>
   );
 }
