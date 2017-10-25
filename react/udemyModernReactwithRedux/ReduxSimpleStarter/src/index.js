4b3f11867c379aa260ddd16026ca802b81cbf4e8
iliandy <iandyli@gmail.com>
2017-10-24 23:45:04 -0500

    Udemy MRwR: integrated search_bar.js, video_detail.js, video_list.js components into index.js.

--- react/udemyModernReactwithRedux/ReduxSimpleStarter/src/index.js
+++ react/udemyModernReactwithRedux/ReduxSimpleStarter/src/index.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
-
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 const API_KEY = "AIzaSyD-sJMng531xBH4TcTuK-pud4OxSg74GrU";
 
-YTSearch({ key: API_KEY, term: "Pixel 2 XL"}, function(data) {
-  console.log(data);
-});
 
 // create component to generate HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: "Pixel 2 XL"}, (videos) => {
+      this.setState({ videos });
+    // this.setState({ videos: videos }), if key == value
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={ this.state.videos[0] } />
+        <VideoList videos={ this.state.videos } />
+      </div>
+    );
+  }
+
 }
 
 // place component generated HTML and put it into page/DOM
