96a3f922614f12ac4b9a6cb1e3f3f4508113922a
iliandy <iandyli@gmail.com>
2017-10-26 21:59:56 -0500

    Udemy MRwR: completed up through 4.44 Actions and Action Creators.

--- react/udemyModernReactwithRedux/ReduxSimpleStarter/src/index.js
+++ react/udemyModernReactwithRedux/ReduxSimpleStarter/src/index.js
@@ -1,51 +1,15 @@
-import React, {Component} from "react";
-import ReactDOM from "react-dom";
-import YTSearch from "youtube-api-search";
-import SearchBar from "./components/search_bar";
-import VideoList from "./components/video_list";
-import VideoDetail from "./components/video_detail";
-import _ from "lodash";
-const API_KEY = "AIzaSyD-sJMng531xBH4TcTuK-pud4OxSg74GrU";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-// create component to generate HTML
-class App extends Component {
-  constructor(props) {
-    super(props);
+import App from './components/app';
+import reducers from './reducers';
 
-    this.state = {
-      videos: [],
-      selectedVideo: null,
-    };
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-    this.videoSearch("Pixel 2 XL");
-  }
-
-  videoSearch(term) {
-    YTSearch({key: API_KEY, term: term}, (videos) => {
-      this.setState({
-        videos: videos,
-        selectedVideo: videos[0],
-      });
-      // this.setState({ videos: videos }), if key == value
-    });
-
-  }
-
-  render() {
-    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
-
-    return (
-      <div>
-        <SearchBar onSearchTermChange={videoSearch} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-          videos={this.state.videos} />
-      </div>
-    );
-  }
-
-}
-
-// place component generated HTML and put it into page/DOM
-ReactDOM.render(<App/>, document.querySelector(".container"));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
---
+++ react/udemyModernReactwithRedux/ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: "Jurassic Park"},
+    {title: "The Truth about Cancer"},
+    {title: "Origin"},
+    {title: "Lord of the Rings: The Fellowship of the Ring"}, 
+  ];
+}
