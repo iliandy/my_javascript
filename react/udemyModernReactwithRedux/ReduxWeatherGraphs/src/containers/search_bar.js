5cab6d26f52e9895fc22e20133fca0c12c95719f
iliandy <iandyli@gmail.com>
2017-10-31 16:11:18 -0500

    Udemy MRwR: completed up through 5.69 Project Review, Weather Graphs project.

--- react/udemyModernReactwithRedux/ReduxWeatherGraphs/src/containers/search_bar.js
+++ react/udemyModernReactwithRedux/ReduxWeatherGraphs/src/containers/search_bar.js
@@ -5,47 +5,40 @@ import {fetchWeather} from "../actions/index";
 
 class SearchBar extends Component {
   constructor(props) {
-  super(props);
+    super(props);
 
-  this.state = {term: ""};
+    this.state = {term: ""};
 
-  this.onInputChange = this.onInputChange.bind(this);
-  this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
-  this.setState({term: event.target.value})
+    this.setState({term: event.target.value})
   }
 
   onFormSubmit(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  // fetch weather data
-  this.props.fetchWeather(this.state.term);
-  this.setState({term: ""});
+    // fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ""});
   }
 
   render() {
-  return (
-    <form className = "input-group"
-      onSubmit = {this.onFormSubmit} >
-    <input className = "form-control"
-    placeholder = "Search cities for five-day forecasts..."
-    value = {
-      this.state.term
-    }
-    onChange = {
-      this.onInputChange
-    }
-    /> <
-    span className = "input-group-btn" >
-    <
-    button type = "submit"
-    className = "btn btn-secondary" > Submit < /button> <
-    /span> <
-    /form>
-  );
-}
+    return (
+      <form className="input-group" onSubmit={this.onFormSubmit}>
+        <input
+          className="form-control"
+          placeholder="Search cities for five-day forecasts..."
+          value={this.state.term}
+          onChange={this.onInputChange} />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    );
+  }
 }
 
 function mapDispatchToProps(dispatch) {
--- react/udemyModernReactwithRedux/ReduxWeatherGraphs/src/reducers/reducer_weather.js
+++ react/udemyModernReactwithRedux/ReduxWeatherGraphs/src/reducers/reducer_weather.js
@@ -1,12 +1,14 @@
 import {FETCH_WEATHER} from "../actions/index";
 
 export default function(state = [], action) {
-  switch (action.type) {
+  switch(action.type) {
     case FETCH_WEATHER:
       // NOT!!! state.push(action.payload.data);
       // return state.concat([action.payload.data]);
       return [action.payload.data, ...state];
   }
+
+
 
   return state;
 }
