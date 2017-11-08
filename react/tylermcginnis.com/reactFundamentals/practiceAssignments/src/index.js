4530d073cd0ec84bfa63e2a7c73f2e23bc9b3578
iliandy <iandyli@gmail.com>
2017-11-08 17:44:55 -0600

    TM.comRF: completed practice assignment 2, props 2

--- react/tylermcginnis.com/reactFundamentals/practiceAssignments/src/index.js
+++ react/tylermcginnis.com/reactFundamentals/practiceAssignments/src/index.js
@@ -2,22 +2,25 @@ import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+var USER_DATA = {
+  name: 'Tyler McGinnis',
+  img: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460',
+  username: 'tylermcginnis'
+}
+
 class Badge extends React.Component {
   render() {
     return (
       <div>
-        <img src={this.props.img} />
-        <h1>Name: {this.props.name}</h1>
-        <h3>username: {this.props.username}</h3>
+        <img src={this.props.user.img} />
+        <h1>Name: {this.props.user.name}</h1>
+        <h3>username: {this.props.user.username}</h3>
       </div>
     )
   }
 }
 
 ReactDOM.render(
-  <Badge
-    name='Tyler McGinnis'
-    username='tylermcginnis'
-    img='https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'/>,
+  <Badge user={USER_DATA}/>,
   document.getElementById('root')
 );
