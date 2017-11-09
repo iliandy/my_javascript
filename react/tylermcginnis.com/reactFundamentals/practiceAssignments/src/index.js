61144ef0d6677ff607c7e8b15bfecaaae73cf56c
iliandy <iandyli@gmail.com>
2017-11-09 17:48:55 -0600

    TM.comRF: completed practice assignment 4, props 4

--- react/tylermcginnis.com/reactFundamentals/practiceAssignments/src/index.js
+++ react/tylermcginnis.com/reactFundamentals/practiceAssignments/src/index.js
@@ -2,47 +2,19 @@ import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Avatar extends React.Component {
+class Users extends React.Component {
   render() {
     return (
-      <img src={this.props.img} />
-    )
-  }
-}
-
-class Label extends React.Component {
-  render() {
-    return (
-      <h1>Name: {this.props.name}</h1>
-    )
-  }
-}
-
-class ScreenName extends React.Component {
-  render() {
-    return (
-      <h3>Username: {this.props.username}</h3>
-    )
-  }
-}
-
-class Badge extends React.Component {
-  render() {
-    return (
-      <div>
-        <Avatar img={this.props.user.img}/>
-        <Label name={this.props.user.name}/>
-        <ScreenName username={this.props.user.username}/>
-      </div>
+      <ul>
+        {this.props.list.map((name) =>
+          <li key={name}> {name} </li>
+        )}
+      </ul>
     )
   }
 }
 
 ReactDOM.render(
-  <Badge user={{
-    name: 'Tyler McGinnis',
-    img: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460',
-    username: 'tylermcginnis'
-  }} />,
+  <Users list={['Tyler', 'Mikenzi', 'Ryan', 'Michael']} />,
   document.getElementById('root')
 );
