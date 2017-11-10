5ffd75a5c938867e886439083f3a1171d3693dbc
iliandy <iandyli@gmail.com>
2017-11-10 17:49:59 -0600

    TM.comRF: completed practice assignment 5, props 5

--- react/tylermcginnis.com/reactFundamentals/practiceAssignments/src/index.js
+++ react/tylermcginnis.com/reactFundamentals/practiceAssignments/src/index.js
@@ -4,17 +4,44 @@ import "./index.css";
 
 class Users extends React.Component {
   render() {
+    const friends = this.props.list.filter((user) => {
+      return user.friend
+    });
+
+    const nonFriends = this.props.list.filter((user) => {
+      return !user.friend
+    });
+
     return (
-      <ul>
-        {this.props.list.map((name) =>
-          <li key={name}> {name} </li>
-        )}
-      </ul>
+      <div>
+        <h1>Friends</h1>
+        <ul>
+          {friends.map((user) =>
+            <li key={user.name}> {user.name} </li>
+          )}
+        </ul>
+
+        <hr />
+
+        <h1>Non Friends</h1>
+        <ul>
+          {nonFriends.map((user) =>
+            <li key={user.name}> {user.name} </li>
+          )}
+        </ul>
+      </div>
     )
   }
 }
 
 ReactDOM.render(
-  <Users list={['Tyler', 'Mikenzi', 'Ryan', 'Michael']} />,
+  <Users list={[
+    { name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false },
+  ]} />,
   document.getElementById('root')
 );
