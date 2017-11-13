fd788fac25477ca401b1adf2f4509d7eccaf7037
iliandy <iandyli@gmail.com>
2017-11-12 18:01:13 -0600

    TM.comRF: completed this keyword and managing and updating component state

--- react/tylermcginnis.com/reactFundamentals/githubBattle/src/components/Popular.js
+++ react/tylermcginnis.com/reactFundamentals/githubBattle/src/components/Popular.js
@@ -10,6 +10,34 @@ class Popular extends Component {
     this.updateLang = this.updateLang.bind(this);
   }
 
+  updateLang(lang) {
+    this.setState(() => {
+      return {
+        selectedLang: lang
+      }
+    });
+  }
 
+
+  render() {
+    const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+
+    console.log(`Selected Language: ${this.state.selectedLang}`);
+    return (
+      <ul className="languages">
+        {languages.map((lang) => {
+          return (
+            <li
+              style={lang === this.state.selectedLang ? {color: "#d0021b"}: null}
+              onClick={this.updateLang.bind(null, lang)}
+              key={lang}>
+                {lang}
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+}
 
 module.exports = Popular;
