c15c8c770eded13bd0c8bf0f685407e2c51b0c8b
iliandy <iandyli@gmail.com>
2017-11-17 17:55:59 -0600

    started App.js component

---
+++ react/tylermcginnis.com/reactFundamentals/githubBattle/src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Nav from "./Nav";
+import Home from "./Home";
+import Battle from "./Battle";
+import Results from "./Results";
+import Popular from "./Popular";
+
+class App extends Component {
+  
+}
+
+export default App;
