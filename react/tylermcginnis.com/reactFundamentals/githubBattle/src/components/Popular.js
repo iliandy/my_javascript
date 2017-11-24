01dcf5f92e158a123ec72701b4830d18dfece35c
iliandy <iandyli@gmail.com>
2017-11-23 18:07:51 -0600

    started Popular.js component

---
+++ react/tylermcginnis.com/reactFundamentals/githubBattle/src/components/Popular.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import api from "../utils/api";
+import Loading from "./Loading";
+
+// Popular component
+class Popular extends Component {
+  
+}
+
+export default Popular;
