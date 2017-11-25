d781aafdacb3bce7b8dd4ebb39bbe346acf5605a
iliandy <iandyli@gmail.com>
2017-11-24 18:08:29 -0600

    completed Popular.js component

--- react/tylermcginnis.com/reactFundamentals/githubBattle/src/components/Popular.js
+++ react/tylermcginnis.com/reactFundamentals/githubBattle/src/components/Popular.js
@@ -5,7 +5,51 @@ import Loading from "./Loading";
 
 // Popular component
 class Popular extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLang: "All",
+      repos: null,
+    };
+
+    this.updateLang = this.updateLang.bind(this);
+  }
+
+  // AJAX requests
+  componentDidMount() {
+    this.updateLang(this.state.selectedLang);
+  }
+
+  updateLang(lang) {
+    this.setState(() => {
+      return {
+        selectedLang: lang,
+        repos: null,
+      }
+    });
+
+    api.fetchPopularRepos(lang)
+      .then((repos) => {
+        this.setState(() => {
+          return {
+            repos: repos
+          }
+        })
+      })
+    }
+
+  render() {
+    console.log(`Selected Language: ${this.state.selectedLang}`);
+    return (
+      <div>
+        <SelectLanguage
+          selectedLang={this.state.selectedLang}
+          onSelect={this.updateLang}
+        />
+        <RepoGrid repos={this.state.repos} />
+      </div>
+    )
+  }
 }
 
 export default Popular;
