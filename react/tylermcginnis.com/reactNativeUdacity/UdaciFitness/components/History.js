4ec2afd832033595a0b13704c105a216f077f683
iliandy <iandyli@gmail.com>
2019-01-28 14:11:47 -0600

    Up through 15.10 Methods, styled renderItem, renderEmptyDate.

--- react/tylermcginnis.com/reactNativeUdacity/UdaciFitness/components/History.js
+++ react/tylermcginnis.com/reactNativeUdacity/UdaciFitness/components/History.js
@@ -1,10 +1,42 @@
 import React, { Component } from "react"
-import { Text, View } from "react-native"
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native"
 import { connect } from "react-redux"
 import { addEntry, receiveEntries } from "../actions"
 import { getDailyReminderValue, timeToString } from "../utils/helpers"
 import { fetchCalendarResults } from "../utils/api"
+import { white } from "../utils/colors"
 import UdaciFitnessCalendar from "udacifitness-calendar"
+import DateHeader from "./DateHeader"
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === "ios" ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: "center",
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: "rgba(0, 0, 0, 0.24)",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+})
 
 class History extends Component {
   componentDidMount = () => {
@@ -26,11 +58,16 @@ class History extends Component {
 
   renderItem = ({ today, ...metrics }, formattedDate, key) => {
     return (
-      <View>
+      <View style={styles.item}>
         {today ? (
-          <Text>{JSON.stringify(today)}</Text>
+          <View>
+            <DateHeader date={formattedDate} />
+            <Text style={styles.noDataText}>{today}</Text>
+          </View>
         ) : (
-          <Text>{JSON.stringify(metrics)}</Text>
+          <TouchableOpacity onPress={() => console.log("Pressed!")}>
+            <Text>{JSON.stringify(metrics)}</Text>
+          </TouchableOpacity>
         )}
       </View>
     )
@@ -38,8 +75,9 @@ class History extends Component {
 
   renderEmptyDate = (formattedDate) => {
     return (
-      <View>
-        <Text>No Data for this day.</Text>
+      <View style={styles.item}>
+        <DateHeader date={formattedDate} />
+        <Text style={styles.noDataText}>No data for this day.</Text>
       </View>
     )
   }
