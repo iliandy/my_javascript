0acab2c4a963580817f342b1bb25a1a9e76190c8
iliandy <iandyli@gmail.com>
2019-01-28 14:31:15 -0600

    Up through 15.11 Metric Card.

---
+++ react/tylermcginnis.com/reactNativeUdacity/UdaciFitness/components/MetricCard.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+import DateHeader from './DateHeader'
+import { getMetricMetaInfo } from '../utils/helpers'
+import { gray } from '../utils/colors'
+
+const styles = StyleSheet.create({
+  metric: {
+    flexDirection: 'row',
+    marginTop: 12
+  },
+}) 
+
+export default MetricCard = ({date, metrics}) => {
+    return (
+      <View>
+        { date && <DateHeader date={date} />}
+        {Object.keys(metrics).map((metric) => {
+          const { backgroundColor, displayName, getIcon, unit } = getMetricMetaInfo()
+
+          return (
+            <View style={styles.metric} key={metric}>
+              {getIcon()}
+              <View>
+                <Text style={{fontSize: 20}}>
+                  {displayName}
+                </Text>
+                <Text style={{fontSize: 16, color: gray}}>
+                  {metrics[metric]} {unit}
+                </Text>
+              </View>
+            <View />
+          )
+        })}
+      </View>
+    )
+  }
+
+const styles = StyleSheet.create({})
