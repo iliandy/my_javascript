e88b40c3e90e706a769369923d13605e599d2b55
iliandy <iandyli@gmail.com>
2019-02-04 17:00:38 -0600

    Up through 22.5 Set Geolocation.

--- react/tylermcginnis.com/reactNativeUdacity/UdaciFitness/components/Live.js
+++ react/tylermcginnis.com/reactNativeUdacity/UdaciFitness/components/Live.js
@@ -6,8 +6,10 @@ import {
   TouchableOpacity,
   View,
 } from "react-native"
+import { Location, Permissions } from "expo"
 import { Foundation } from "@expo/vector-icons"
 import { blue, purple, white } from "../utils/colors"
+import { calculateDirection } from "../utils/helpers"
 
 const styles = StyleSheet.create({
   container: {
@@ -71,10 +73,46 @@ export default class Live extends Component {
   state = {
     coords: null,
     direction: "",
-    status: "granted",
+    status: null,
+  }
+
+  componentDidMount() {
+    Permissions.getAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === "granted") {
+          return this.setLocation()
+        }
+
+        this.setState(() => ({ status }))
+      })
+      .catch((error) => {
+        console.warn("Error getting location permission: ", error)
+
+        this.setState(() => ({ status: "undetermined" }))
+      })
   }
 
   askPermission = () => {}
+
+  setLocation = () => {
+    Location.watchPositionAsync(
+      {
+        enableHighAccuracy: true,
+        timeInterval: 1,
+        distanceInterval: 1,
+      },
+      ({ coords }) => {
+        const newDirection = calculateDirection(coords.heading)
+        const { direction } = this.state
+
+        this.setState(() => ({
+          coords,
+          status: granted,
+          direction: newDirection,
+        }))
+      }
+    )
+  }
 
   render() {
     const { coords, direction, status } = this.state
