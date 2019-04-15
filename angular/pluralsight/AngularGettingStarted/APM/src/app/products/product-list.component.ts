1cd74e02c154faf350a9928724845211123591e2
Andy Li <iandyli@gmail.com>
2019-04-15 17:07:43 -0500

    Up through 7.4 Using Lifecycle Hooks.

--- angular/pluralsight/AngularGettingStarted/APM/src/app/products/product-list.component.ts
+++ angular/pluralsight/AngularGettingStarted/APM/src/app/products/product-list.component.ts
@@ -1,12 +1,12 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
 import { IProduct } from "./product"
 
 @Component({
   selector: "pm-products",
   templateUrl: "./product-list.component.html",
-  // styleUrls: ['./product-list.component.css'],
+  styleUrls: ["./product-list.component.css"],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   imageWidth: number = 50
   imageMargin: number = 2
   listFilter: string = ""
@@ -39,5 +39,9 @@ export class ProductListComponent {
 
   toggleImage = (): void => {
     this.showImage = !this.showImage
+  }
+
+  ngOnInit(): void {
+    console.log("In OnInit")
   }
 }
