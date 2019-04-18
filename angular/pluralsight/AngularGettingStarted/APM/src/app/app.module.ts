3594dfa2677dc72edecf1960f5f8a8775573e768
Andy Li <iandyli@gmail.com>
2019-04-18 12:06:37 -0500

    Up through/completed 12. Navigation and Routing Additional Techniques.

--- angular/pluralsight/AngularGettingStarted/APM/src/app/app.module.ts
+++ angular/pluralsight/AngularGettingStarted/APM/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { RouterModule } from "@angular/router"
 
 import { AppComponent } from "./app.component"
 import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe"
+import { ProductDetailComponent } from "./products/product-detail.component"
+import { ProductDetailGuard } from "./products/product-detail.guard"
 import { ProductListComponent } from "./products/product-list.component"
 import { StarComponent } from "./shared/star.component"
-import { ProductDetailComponent } from "./products/product-detail.component"
 import { WelcomeComponent } from "./home/welcome.component"
 
 @NgModule({
@@ -26,7 +27,11 @@ import { WelcomeComponent } from "./home/welcome.component"
     HttpClientModule,
     RouterModule.forRoot([
       { path: "products", component: ProductListComponent },
-      { path: "products/:id", component: ProductDetailComponent },
+      {
+        path: "products/:id",
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent,
+      },
       { path: "welcome", component: WelcomeComponent },
       { path: "", redirectTo: "welcome", pathMatch: "full" },
       { path: "**", redirectTo: "welcome", pathMatch: "full" },
