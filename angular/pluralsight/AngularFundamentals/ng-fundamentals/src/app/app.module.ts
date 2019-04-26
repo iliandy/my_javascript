2cb3f47deb55d5e5515d62783209eac0a3975af5
Andy Li <iandyli@gmail.com>
2019-04-26 16:23:23 -0500

    Completed 3. Creating and Communicating Between Angular Components

--- angular/pluralsight/AngularFundamentals/ng-fundamentals/src/app/app.module.ts
+++ angular/pluralsight/AngularFundamentals/ng-fundamentals/src/app/app.module.ts
@@ -4,9 +4,15 @@ import { NgModule } from "@angular/core"
 import { AppComponent } from "./app.component"
 import { EventsComponent } from "./components/events/events.component"
 import { EventThumbnailComponent } from "./components/event-thumbnail/event-thumbnail.component"
+import { NavbarComponent } from "./components/navbar/navbar.component"
 
 @NgModule({
-  declarations: [AppComponent, EventsComponent, EventThumbnailComponent],
+  declarations: [
+    AppComponent,
+    EventsComponent,
+    EventThumbnailComponent,
+    NavbarComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
