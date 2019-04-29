a3c765f9f1ca49e30691cef0690354ead519a8e1
Andy Li <iandyli@gmail.com>
2019-04-29 16:32:13 -0500

    Completed 4. Exploring the Angular Template Syntax

--- angular/pluralsight/AngularFundamentals/ng-fundamentals/src/app/components/event-thumbnail/event-thumbnail.component.ts
+++ angular/pluralsight/AngularFundamentals/ng-fundamentals/src/app/components/event-thumbnail/event-thumbnail.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Input } from "@angular/core"
 @Component({
   selector: "event-thumbnail",
   templateUrl: "./event-thumbnail.component.html",
+  styleUrls: ["./event-thumbnail.component.css"],
 })
 export class EventThumbnailComponent implements OnInit {
   @Input() event: any
@@ -10,4 +11,11 @@ export class EventThumbnailComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  getStartTimeStyle(): any {
+    if (this.event && this.event.time === "8:00 am") {
+      return { color: "#003300", "font-weight": "bold" }
+    }
+    return {}
+  }
 }
