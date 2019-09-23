5e76d61cac079863895b1ce31a5c8f3bdbf97f28
Andy Li <iandyli@gmail.com>
2019-09-22 23:09:16 -0500

    Implemented HTTP GET for all books and specific book.

--- node/pluralsight/RESTfulWebServicesWithNodeExpress/app.js
+++ node/pluralsight/RESTfulWebServicesWithNodeExpress/app.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Book = require('./models/bookModel');
 
 const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPI');
 const port = process.env.PORT || 3000;
+const router = express.Router();
 
 app.get('/', (req, res) => {
   res.send('Hello Nodemon API!');
+});
+
+app.use('/api', router);
+
+router.route('/books').get((req, res) => {
+  const query = {};
+
+  if (req.query.genre) {
+    query.genre = req.query.genre;
+  }
+
+  Book.find(query, (error, books) => {
+    return error ? res.send(error) : res.json(books);
+  });
+});
+
+router.route('/books/:bookId').get((req, res) => {
+  Book.findById(req.params.bookId, (error, book) => {
+    return error ? res.send(error) : res.json(book);
+  });
 });
 
 app.listen(port, () => {
